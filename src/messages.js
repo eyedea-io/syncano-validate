10420d2728d0aec8158cbf8ae9e5e60fef4471e8
Kasper Mikiewicz <ideredpl@gmail.com>
2016-12-20 16:45:04 +0100

    refactor: change messages json to js

---
+++ src/messages.js
@@ -0,0 +1,10 @@
+export default {
+  required: 'The :attribute field is required.',
+  min: {
+    numeric: 'The :attribute must be at least :min.',
+    string: 'The :attribute must be at least :min characters.',
+    array: 'The :attribute must have at least :min items.'
+  },
+  exists: 'The selected :attribute is invalid.',
+  in: 'The selected :attribute is invalid.'
+}

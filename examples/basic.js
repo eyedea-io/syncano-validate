88c35ad48bc0ffc106b411c69aeb30b61655dd7d
Kasper Mikiewicz <ideredpl@gmail.com>
2017-08-22 18:13:40 +0200

    feat: implement array validation and custom messages

--- examples/basic.js
+++ examples/basic.js
@@ -4,18 +4,24 @@ export default () => {
   const data = {
     firstName: 'John',
     lastName: 'Doe',
-    age: 16,
+    age: [16, 24],
     gender: 'male'
   }
 
   const rules = {
     firstName: 'required|min:2',
     lastName: 'required|min:2',
-    age: 'required|numeric|min:18',
-    gender: 'in:male,female|exists:tag,name'
+    'age.*': 'required|numeric|min:18'
+    // gender: 'in:male,female|exists:tag,name'
   }
 
-  validate(data, rules)
+  const messages = {
+    'age.*': {
+      min: 'Every age must be at least :min'
+    }
+  }
+
+  validate(data, rules, messages)
     .then(() => console.log())
     .catch(console.log)
 }
